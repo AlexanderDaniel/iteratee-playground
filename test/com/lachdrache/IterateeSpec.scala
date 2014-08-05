1244d77f13c7840a21e237f1c346f59bc809a42d
Alexander Daniel <alexander.daniel@gmx.at>
2014-08-05 13:44:20 +0200

    summing via iteratee

--- test/com/lachdrache/IterateeSpec.scala
+++ test/com/lachdrache/IterateeSpec.scala
@@ -1,7 +1,8 @@
 package com.lachdrache
 
 import org.scalatest.FunSpec
-import play.api.libs.iteratee.{Input, Iteratee, Step}
+import play.api.libs.iteratee.Step.Done
+import play.api.libs.iteratee.{Enumerator, Input, Iteratee, Step}
 import play.api.test.Helpers._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -29,6 +30,20 @@ class IterateeSpec extends FunSpec {
         await(eventuallyMaybeResult)
       }
     }
-
   }
+
+  // http://mandubian.com/2012/08/27/understanding-play2-iteratees-for-normal-humans/
+  describe("summing iteratee") {
+    it("using Iteratee.fold to calculate the sum via fold") {
+      val enumerator: Enumerator[Int] = Enumerator(1, 2, 3, 4, 5)
+      assertResult(15) {
+        val future: Future[Int] = enumerator.run(
+          Iteratee.fold(0) { (total, el) => total + el}
+        )
+        await(future)
+      }
+    }
+  }
+
+
 }
